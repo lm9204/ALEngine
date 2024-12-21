1f2dd030ec80c9a3bd78f81f41bd36a2eab9d023
jgoo <maruluv0527@gmail.com>
2024-12-21 17:08:52 +0900

    [Feat]: added namespace

---
+++ AL/include/Core/Application.h
@@ -0,0 +1,14 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "Core/Base.h"
+
+namespace ale
+{
+    class Application
+    {
+
+    };
+}
+
+#endif
---
+++ AL/include/alpch.h
@@ -0,0 +1,9 @@
+#ifndef ALPCH_H
+#define ALPCH_H
+
+// precompiled header
+#include <iostream>
+#include <vector>
+
+
+#endif
