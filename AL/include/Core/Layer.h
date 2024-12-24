db36d9747735bdea3f01057ce075b0413524085a
jgoo <maruluv0527@gmail.com>
2024-12-24 21:29:52 +0900

    [Feat]: layer class added

---
+++ AL/include/Core/Layer.h
@@ -0,0 +1,31 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+#include "Core/Base.h"
+#include "Events/Event.h"
+
+namespace ale
+{
+class AL_API Layer
+{
+  public:
+	Layer(const std::string &name = "Layer");
+	virtual ~Layer() = default;
+
+	virtual void onAttach();
+	virtual void onDetach();
+	virtual void onUpdate();
+	virtual void onImGuiRender();
+	virtual void onEvent(Event &event);
+
+	const std::string &getName() const
+	{
+		return m_DebugName;
+	}
+
+  private:
+	std::string m_DebugName;
+};
+} // namespace ale
+
+#endif
