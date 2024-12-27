64f9d3871f6791a67d3260f0a4ee1cfd84469a16
jgoo <maruluv0527@gmail.com>
2024-12-27 10:56:34 +0900

    [Feat]: Models class added

---
+++ AL/include/Renderer/Model.h
@@ -0,0 +1,40 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "Core/Base.h"
+#include "Renderer/Common.h"
+#include "Renderer/Mesh.h"
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+namespace ale
+{
+class AL_API Model
+{
+  public:
+	static std::shared_ptr<Model> createModel(std::string path);
+	static std::shared_ptr<Model> createBoxModel();
+	static std::shared_ptr<Model> createSphereModel();
+	static std::shared_ptr<Model> createPlaneModel();
+	~Model() = default;
+	void cleanup();
+
+	void draw(VkCommandBuffer commandBuffer);
+
+  private:
+	Model() = default;
+
+	std::vector<std::shared_ptr<Mesh>> m_meshes;
+
+	void initModel(std::string path);
+	void initBoxModel();
+	void initSphereModel();
+	void initPlaneModel();
+	void loadModel(std::string path);
+	void processNode(aiNode *node, const aiScene *scene);
+	std::shared_ptr<Mesh> processMesh(aiMesh *mesh, const aiScene *scene);
+};
+
+} // namespace ale
+
+#endif
