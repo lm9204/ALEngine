6a5c7b6b7b2365a37a6d877c06aff871b518abba
jgoo <maruluv0527@gmail.com>
2024-12-29 17:24:57 +0900

    [Feat]: ImGui init added

--- AL/include/ImGui/ImGuiLayer.h
+++ AL/include/ImGui/ImGuiLayer.h
@@ -1,7 +1,12 @@
 #ifndef IMGUILAYER_H
 #define IMGUILAYER_H
 
+#include "Core/App.h"
 #include "Core/Layer.h"
+#include "Platform/Vulkan/ImGuiVulkanGlfw.h"
+#include "Platform/Vulkan/ImGuiVulkanRenderer.h"
+#include "Renderer/Common.h"
+#include "Renderer/VulkanContext.h"
 
 namespace ale
 {
@@ -16,7 +21,14 @@ class AL_API ImGuiLayer : public Layer
 	void onUpdate() override;
 	void onEvent(Event &event) override;
 
+	bool show_demo_window = false;
+
   private:
+	VkCommandBuffer beginSingleTimeCommands();
+	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
+
+	ImGui_ImplVulkan_InitInfo init_info = {};
+	VkCommandPool commandPool;
 };
 } // namespace ale
 
