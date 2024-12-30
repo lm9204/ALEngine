440426fec548ffb947f6d7ff58be0c332b364554
jgoo <maruluv0527@gmail.com>
2024-12-30 22:50:27 +0900

    [Feat]: onImGuiRender, renderDrawData added

--- AL/include/ImGui/ImGuiLayer.h
+++ AL/include/ImGui/ImGuiLayer.h
@@ -1,10 +1,14 @@
 #ifndef IMGUILAYER_H
 #define IMGUILAYER_H
 
-#include "Core/App.h"
 #include "Core/Layer.h"
-#include "Platform/Vulkan/ImGuiVulkanGlfw.h"
-#include "Platform/Vulkan/ImGuiVulkanRenderer.h"
+#include "Events/AppEvent.h"
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
+
+#include "ImGui/ImGuiVulkanGlfw.h"
+#include "ImGui/ImGuiVulkanRenderer.h"
+
 #include "Renderer/Common.h"
 #include "Renderer/VulkanContext.h"
 
@@ -18,8 +22,11 @@ class AL_API ImGuiLayer : public Layer
 
 	void onAttach() override;
 	void onDetach() override;
-	void onUpdate() override;
 	void onEvent(Event &event) override;
+	void onImGuiRender() override;
+
+	void begin();
+	static void renderDrawData(VkCommandBuffer commandBuffer);
 
 	bool show_demo_window = false;
 
@@ -27,6 +34,7 @@ class AL_API ImGuiLayer : public Layer
 	VkCommandBuffer beginSingleTimeCommands();
 	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
 
+  private:
 	ImGui_ImplVulkan_InitInfo init_info = {};
 	VkCommandPool commandPool;
 };
