d8b3f8da57efb8095c43c9de1ab6f2c552f9549c
jgoo <maruluv0527@gmail.com>
2025-01-12 21:42:35 +0900

    [Feat]: addComponent fixed, operator added

--- AL/include/Scene/Entity.h
+++ AL/include/Scene/Entity.h
@@ -15,7 +15,9 @@ class Entity
 	template <typename T, typename... Args> T &addComponent(Args &&...args)
 	{
 		// ASSERT NOT HAS COMPONENT
-		return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+		T &component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+		m_Scene->onComponentAdded<T>(*this, component);
+		return component;
 	}
 
 	template <typename T, typename... Args> T &addOrReplaceComponent(Args &&...args)
@@ -39,6 +41,29 @@ class Entity
 	template <typename T> bool hasComponent()
 	{
 		return m_Scene->m_Registry.all_of<T>(m_EntityHandle);
+	}
+
+	operator bool() const
+	{
+		return m_EntityHandle != entt::null;
+	}
+	operator entt::entity() const
+	{
+		return m_EntityHandle;
+	}
+	operator uint32_t() const
+	{
+		return (uint32_t)m_EntityHandle;
+	}
+
+	bool operator==(const Entity &other) const
+	{
+		return m_EntityHandle == other.m_EntityHandle && m_Scene == other.m_Scene;
+	}
+
+	bool operator!=(const Entity &other) const
+	{
+		return !(*this == other);
 	}
 
   private:
