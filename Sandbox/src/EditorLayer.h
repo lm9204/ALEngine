ae42951a1a50327644de4001052acff0c7b86fe3
jgoo <maruluv0527@gmail.com>
2025-01-13 14:24:55 +0900

    [Feat]: serializer flow added

--- Sandbox/src/EditorLayer.h
+++ Sandbox/src/EditorLayer.h
@@ -19,8 +19,27 @@ class EditorLayer : public Layer
 	void onImGuiRender() override;
 	void onEvent(Event &e) override;
 
+  private:
+	// EVENTS
+	bool onMouseButtonPressed(MouseButtonPressedEvent &e);
+	bool onKeyPressed(KeyPressedEvent &e);
+
+	// GUI
 	void setDockingSpace();
 	void setMenuBar();
+
+	// PROJECT
+	void newProject();
+	void openProject(const std::filesystem::path &path);
+	bool openProject();
+	void saveProject();
+
+	// SCENE
+	void newScene();
+	void openScene();
+	void openScene(const std::filesystem::path &path);
+	void saveScene();
+	void saveSceneAs();
 
   private:
 	CameraController m_CameraController;
