dc478934559a638a1bbf5848fb33514cd218548a
jgoo <maruluv0527@gmail.com>
2025-01-14 16:11:25 +0900

    [Refactoring]: Camera class refactored

--- AL/include/Renderer/Camera.h
+++ AL/include/Renderer/Camera.h
@@ -15,6 +15,7 @@ class Camera
 	virtual ~Camera() = default;
 
 	void setProjMatrix(float fov, float aspect, float _near, float _far);
+	void setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up);
 	void setPosition(glm::vec3 &pos);
 
 	glm::vec3 &getPosition()
@@ -27,10 +28,14 @@ class Camera
 		return m_Projection;
 	}
 
+	const glm::mat4 &getView() const
+	{
+		return m_View;
+	}
+
   protected:
 	glm::mat4 m_Projection = glm::mat4(1.0f);
-
-  private:
+	glm::mat4 m_View;
 	glm::vec3 m_Position{0.0f, 0.0f, 10.0f};
 };
 } // namespace ale
--- AL/src/Renderer/Camera.cpp
+++ AL/src/Renderer/Camera.cpp
@@ -8,6 +8,11 @@ void Camera::setProjMatrix(float fov, float aspect, float _near, float _far)
 	m_Projection = glm::perspective(fov, aspect, _near, _far);
 }
 
+void Camera::setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up)
+{
+	m_View = glm::lookAt(pos, pos + dir, up);
+}
+
 void Camera::setPosition(glm::vec3 &pos)
 {
 	m_Position = pos;
