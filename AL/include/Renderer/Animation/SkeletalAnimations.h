a2a0d371786612fefe14d61c57975216bb24334c
Ryan <vapor2008@naver.com>
2025-01-22 21:35:17 +0900

    [Feat]: add SkeletalAnimations (Group)

---
+++ AL/include/Renderer/Animation/SkeletalAnimations.h
@@ -0,0 +1,60 @@
+#ifndef SKELETALANIMATIONS_H
+#define SKELETALANIMATIONS_H
+
+#include <map>
+
+#include "SkeletalAnimation.h"
+
+namespace ale
+{
+class SkeletalAnimations
+{
+	public:
+		using pSkeletalAnimation = std::shared_ptr<SkeletalAnimation>;
+
+		struct Iterator
+		{
+			Iterator(pSkeletalAnimation* pointer);
+			Iterator& operator++();
+			bool operator!=(const Iterator& rightHandSide);
+			SkeletalAnimation& operator*();
+
+		private:
+			pSkeletalAnimation* m_Pointer;
+		};
+
+	public:
+		Iterator begin();
+		Iterator end();
+		SkeletalAnimation& operator[](std::string const& animation);
+		SkeletalAnimation& operator[](uint16_t index);
+
+	public:
+		SkeletalAnimations();
+
+		size_t size() const { return m_Animations.size(); }
+		void push(std::shared_ptr<SkeletalAnimation> const& animation);
+
+		void start(std::string const& animation); // by name
+		void start(size_t index);                 // by index
+		void start() { start(0); };               // start animation 0
+		void stop();
+		void setRepeat(bool repeat);
+		void setRepeatAll(bool repeat);
+		bool isRunning() const;
+		bool willExpire(const Timestep& timestep) const;
+		float getDuration(std::string const& animation);
+		float getCurrentTime();
+		std::string getName();
+		void update(const Timestep& timestep, Armature::Skeleton& skeleton, uint16_t frameCounter);
+		int getIndex(std::string const& animation);
+
+	private:
+		std::map<std::string, std::shared_ptr<SkeletalAnimation>> m_Animations;
+		std::vector<std::shared_ptr<SkeletalAnimation>> m_AnimationsVector;
+		SkeletalAnimation* m_CurrentAnimation;
+		uint16_t m_FrameCounter;
+		std::map<std::string, int> m_NameToIndex;
+};
+}
+#endif
