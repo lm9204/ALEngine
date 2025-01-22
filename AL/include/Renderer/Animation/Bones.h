09ed8095331bc1d3e0c17ceb383763ae711b781c
Ryan <vapor2008@naver.com>
2025-01-22 21:34:51 +0900

    [Feat]: add Skeleton, Bone structure

---
+++ AL/include/Renderer/Animation/Bones.h
@@ -0,0 +1,7 @@
+#ifndef BONES_H
+#define BONES_H
+
+#define MAX_BONES 100
+#define MAX_BONE_INFLUENCE 4
+
+#endif
---
+++ AL/include/Renderer/Animation/Skeleton.h
@@ -0,0 +1,62 @@
+#ifndef SKELETON_H
+#define SKELETON_H
+
+#include "Renderer/Common.h"
+#include <iostream>
+#include <map>
+
+#include "Bones.h"
+
+namespace ale
+{
+	namespace Armature
+	{
+		static constexpr int NO_PARENT = -1;
+		static constexpr int ROOT_JOINT = 0;
+
+		struct ShaderData
+		{
+			std::vector<glm::mat4> m_FinalBonesMatrices;
+		};
+
+		// TODO : 최적화 ->  동적/정적 데이터 분리
+
+		struct Bone
+		{
+			// TODO 동적/정적 데이터 분리사항
+				// glm::mat4 m_BindPose{0.0f};
+				// glm::mat4 m_InverseBindPose{0.0f};
+
+			std::string m_Name;
+			glm::mat4 m_InverseBindMatrix;
+
+			// deformed data
+			glm::vec3 m_DeformedNodeTranslation{0.0f};
+			glm::quat m_DeformedNodeRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+			glm::vec3 m_DeformedNodeScale{1.0f};
+
+			glm::mat4 getDeformedBindMatrix()
+			{
+				return	glm::translate(glm::mat4(1.0f), m_DeformedNodeTranslation) *	// T
+						glm::mat4(m_DeformedNodeRotation) *								// R
+						glm::scale(glm::mat4(1.0f), m_DeformedNodeScale);				// S
+			}
+
+			int m_ParentBone;
+			std::vector<int> m_Children;
+		};
+
+		struct Skeleton
+		{
+			void update();
+			void updateBone(int16_t boneIndex);
+
+			bool m_isAnimated = true;
+			std::string m_Name;
+			std::vector<Bone> m_Bones;
+			std::map<int, int> m_GlobalNodeToBoneIndex;
+			ShaderData m_ShaderData;
+		};
+	};
+}
+#endif
