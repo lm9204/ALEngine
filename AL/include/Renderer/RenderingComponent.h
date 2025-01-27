6772435713b70bb88eac5f4ae6286d7542742872
jgoo <maruluv0527@gmail.com>
2025-01-27 22:16:25 +0900

    [Feat]: Merging Renderer

---
+++ AL/include/Renderer/RenderingComponent.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Core/Base.h"
+#include "Renderer/Common.h"
+#include "Renderer/Model.h"
+#include "Renderer/ShaderResourceManager.h"
+
+namespace ale
+{
+class RenderingComponent
+{
+  public:
+	static std::unique_ptr<RenderingComponent> createRenderingComponent(std::shared_ptr<Model> model);
+	~RenderingComponent() = default;
+
+	void draw(DrawInfo &drawInfo);
+	void drawShadow(ShadowMapDrawInfo &drawInfo, uint32_t index);
+	void drawShadowCubeMap(ShadowCubeMapDrawInfo &drawInfo, uint32_t index);
+	void updateMaterial(std::vector<std::shared_ptr<Material>> materials);
+
+	std::vector<std::shared_ptr<Material>> &getMaterials()
+	{
+		return m_materials;
+	}
+
+  public:
+	enum class EPrimitiveType
+	{
+		BOX = 0,
+		SPHERE,
+		PLANE,
+		MODEL,
+		NONE
+	};
+
+  private:
+	RenderingComponent() = default;
+	std::shared_ptr<Model> m_model;
+	std::unique_ptr<ShaderResourceManager> m_shaderResourceManager;
+	std::vector<std::unique_ptr<ShaderResourceManager>> m_shadowMapResourceManager;
+	std::vector<std::unique_ptr<ShaderResourceManager>> m_shadowCubeMapResourceManager;
+	std::vector<std::shared_ptr<Material>> m_materials;
+	void initRenderingComponent(std::shared_ptr<Model> model);
+};
+
+} // namespace ale
