369408f49e0093fd1562be7befb944d3d1648757
Ryan <vapor2008@naver.com>
2025-01-28 15:48:28 +0900

    [Feat]: Add Animations Speed Factor & Add Animations Member Variable

--- AL/src/Renderer/SAComponent.cpp
+++ AL/src/Renderer/SAComponent.cpp
@@ -2,16 +2,20 @@
 
 namespace ale
 {
-SAComponent::SAComponent()
-{
 
+SAComponent::SAComponent() : m_Repeat(0), m_FirstKeyFrameTime(0), m_CurrentKeyFrameTime(0), m_LastKeyFrameTime(0), m_FrameCounter(0), m_SpeedFactor(1.0f)
+{
+	m_CurrentAnimation = nullptr;
 }
 
 void SAComponent::updateAnimation(const Timestep& timestep, uint32_t currentFrame)
 {
-	m_CurrentAnimation->uploadData(this->getData());
-	m_Model->updateAnimations(timestep, currentFrame);
-	this->setData(m_Model->getAnimCurrentFrame() ,m_CurrentAnimation->getData());
+	if (m_CurrentAnimation)
+	{
+		m_CurrentAnimation->uploadData(this->getData());
+		m_Model->updateAnimations(m_CurrentAnimation, timestep, m_SpeedFactor, m_FrameCounter, currentFrame);
+		this->setData(m_Model->getAnimCurrentFrame() ,m_CurrentAnimation->getData());
+	}
 }
 
 struct SAData SAComponent::getData() const
