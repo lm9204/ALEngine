cfdf68a0311a5e7925dd838a5bc0f60385d0dbef
jgoo <maruluv0527@gmail.com>
2025-02-04 19:16:37 +0900

    [Feat]: Shape Collisions header added

---
+++ AL/include/Physics/Shape/ShapeCollisions.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Physics/Contact/SphereToBoxContact.h"
+#include "Physics/Contact/SphereToCapsuleContact.h"
+#include "Physics/Contact/SphereToCylinderContact.h"
+#include "Physics/Contact/SphereToSphereContact.h"
+
+#include "Physics/Contact/BoxToBoxContact.h"
+#include "Physics/Contact/BoxToCapsuleContact.h"
+#include "Physics/Contact/BoxToCylinderContact.h"
+
+#include "Physics/Contact/CylinderToCapsuleContact.h"
+#include "Physics/Contact/CylinderToCylinderContact.h"
+
+#include "Physics/Contact/CapsuleToCapsuleContact.h"
