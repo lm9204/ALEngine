b75bd945f52bc2f259cf8ff86bbbf7b5ffd2e732
jgoo <maruluv0527@gmail.com>
2025-02-04 19:16:19 +0900

    [Feat]: Shape class added

---
+++ AL/src/Physics/Shape/Shape.cpp
@@ -0,0 +1,9 @@
+#include "Physics/Shape/Shape.h"
+
+namespace ale
+{
+int32_t operator|(EType type1, EType type2)
+{
+	return static_cast<int32_t>(type1) | static_cast<int32_t>(type2);
+}
+} // namespace ale
