542709b96f308b10f77d23d0378729b0c707193a
seonjo1 <tjdgus9684@gmail.com>
2025-02-05 17:50:36 +0900

    [Fix]: update Camera

--- AL/include/Renderer/Camera.h
+++ AL/include/Renderer/Camera.h
@@ -12,31 +12,38 @@ class Camera
 {
   public:
 	Camera() = default;
+	Camera(float fov, float aspect, float near, float far);
 	virtual ~Camera() = default;
 
+	void setFov(float fov);
+	void setNear(float _near);
+	void setFar(float _far);
 	void setProjMatrix(float fov, float aspect, float _near, float _far);
 	void setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up);
+	void setViewPortSize(uint32_t width, uint32_t height);
 	void setPosition(glm::vec3 &pos);
 
-	glm::vec3 &getPosition()
-	{
-		return m_Position;
-	}
+	float getFov() const;
+	float getNear() const;
+	float getFar() const;
+	glm::vec3 &getPosition();
+	const glm::mat4 &getProjection() const;
+	const glm::mat4 &getView() const;
 
-	const glm::mat4 &getProjection() const
-	{
-		return m_Projection;
-	}
-
-	const glm::mat4 &getView() const
-	{
-		return m_View;
-	}
+	void updateProjMatrix();
 
   protected:
-	glm::mat4 m_Projection = glm::mat4(1.0f);
-	glm::mat4 m_View;
-	glm::vec3 m_Position{0.0f, 0.0f, 10.0f};
+	glm::mat4 m_projection = glm::mat4(1.0f);
+	glm::mat4 m_view;
+
+	glm::vec3 m_cameraPos{0.0f, 0.0f, 10.0f};
+	glm::vec3 m_cameraFront{0.0f, 0.0f, -1.0f};
+	glm::vec3 m_cameraUp{0.0f, 1.0f, 0.0f};
+	
+	float m_fov = glm::radians(45.0f);
+	float m_near = 0.001f;
+	float m_aspect = (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT;
+	float m_far = 1000.0f;
 };
 } // namespace ale
 
--- AL/src/Renderer/Camera.cpp
+++ AL/src/Renderer/Camera.cpp
@@ -3,19 +3,89 @@
 
 namespace ale
 {
+Camera::Camera(float fov, float aspect, float _near, float _far)
+{
+	setProjMatrix(fov, aspect, _near, _far);
+	setViewMatrix(m_cameraPos, m_cameraFront, m_cameraUp);
+}
+
 void Camera::setProjMatrix(float fov, float aspect, float _near, float _far)
 {
-	m_Projection = glm::perspective(fov, aspect, _near, _far);
+	m_fov = fov;
+	m_aspect = aspect;
+	m_near = _near;
+	m_far = _far;
+
+	m_projection = glm::perspective(fov, aspect, _near, _far);
 }
 
 void Camera::setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up)
 {
-	m_View = glm::lookAt(pos, pos + dir, up);
+	m_view = glm::lookAt(pos, pos + dir, up);
+}
+
+void Camera::setViewPortSize(uint32_t width, uint32_t height)
+{
+	m_aspect = static_cast<float>(width) / static_cast<float>(height);
+	updateProjMatrix();
+}
+
+void Camera::updateProjMatrix()
+{
+	m_projection = glm::perspective(m_fov, m_aspect, m_near, m_far);
 }
 
 void Camera::setPosition(glm::vec3 &pos)
 {
-	m_Position = pos;
+	m_cameraPos = pos;
+}
+
+void Camera::setFov(float fov)
+{
+	m_fov = fov;
+	updateProjMatrix();
+}
+
+void Camera::setNear(float _near)
+{
+	m_near = _near;
+	updateProjMatrix();
+}
+
+void Camera::setFar(float _far)
+{
+	m_far = _far;
+	updateProjMatrix();
+}
+
+float Camera::getFov() const
+{
+	return m_fov;
+}
+
+float Camera::getNear() const
+{
+	return m_near;
+}
+
+float Camera::getFar() const
+{
+	return m_far;
+}
+
+glm::vec3 &Camera::getPosition()
+{
+	return m_cameraPos;
+}
+
+const glm::mat4 &Camera::getProjection() const
+{
+	return m_projection;
+}
+
+const glm::mat4 &Camera::getView() const
+{
+	return m_view;
 }
 
 } // namespace ale
