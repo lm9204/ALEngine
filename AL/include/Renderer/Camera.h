a234e693d31617dcc0fd8e13b24366ad511f5e9e
seonjo1 <tjdgus9684@gmail.com>
2025-02-06 03:07:58 +0900

    [Feat]: add Frustum in Camera

--- AL/include/Renderer/Camera.h
+++ AL/include/Renderer/Camera.h
@@ -8,6 +8,8 @@
 namespace ale
 {
 
+struct Frustum;
+
 class Camera
 {
   public:
@@ -20,12 +22,13 @@ class Camera
 	void setFar(float _far);
 	void setProjMatrix(float fov, float aspect, float _near, float _far);
 	void setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up);
-	void setViewPortSize(uint32_t width, uint32_t height);
+	void setViewportSize(uint32_t width, uint32_t height);
 	void setPosition(glm::vec3 &pos);
 
 	float getFov() const;
 	float getNear() const;
 	float getFar() const;
+	const Frustum &getFrustum();
 	glm::vec3 &getPosition();
 	const glm::mat4 &getProjection() const;
 	const glm::mat4 &getView() const;
@@ -39,7 +42,8 @@ class Camera
 	glm::vec3 m_cameraPos{0.0f, 0.0f, 10.0f};
 	glm::vec3 m_cameraFront{0.0f, 0.0f, -1.0f};
 	glm::vec3 m_cameraUp{0.0f, 1.0f, 0.0f};
-	
+
+	Frustum m_frustum;
 	float m_fov = glm::radians(45.0f);
 	float m_near = 0.001f;
 	float m_aspect = (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT;
--- AL/src/Renderer/Camera.cpp
+++ AL/src/Renderer/Camera.cpp
@@ -1,8 +1,10 @@
 #include "Renderer/Camera.h"
 #include "ALpch.h"
+#include "Scene/CullTree.h"
 
 namespace ale
 {
+
 Camera::Camera(float fov, float aspect, float _near, float _far)
 {
 	setProjMatrix(fov, aspect, _near, _far);
@@ -16,7 +18,7 @@ void Camera::setProjMatrix(float fov, float aspect, float _near, float _far)
 	m_near = _near;
 	m_far = _far;
 
-	m_projection = glm::perspective(fov, aspect, _near, _far);
+	m_projection = glm::perspective(m_fov, m_aspect, m_near, m_far);
 }
 
 void Camera::setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up)
@@ -24,7 +26,7 @@ void Camera::setViewMatrix(glm::vec3 &pos, glm::vec3 &dir, glm::vec3 &up)
 	m_view = glm::lookAt(pos, pos + dir, up);
 }
 
-void Camera::setViewPortSize(uint32_t width, uint32_t height)
+void Camera::setViewportSize(uint32_t width, uint32_t height)
 {
 	m_aspect = static_cast<float>(width) / static_cast<float>(height);
 	updateProjMatrix();
@@ -86,6 +88,50 @@ const glm::mat4 &Camera::getProjection() const
 const glm::mat4 &Camera::getView() const
 {
 	return m_view;
+}
+
+const Frustum &Camera::getFrustum()
+{
+	glm::mat4 toWorldMatrix = glm::inverse(m_view);
+
+	// 0: center, 1: leftUp, 2: leftDown, 3: rightDown, 4: rightUp
+	glm::vec3 farPoint[5];
+	glm::vec3 nearPoint[5];
+
+	// 0: Y, 1: X
+	glm::vec3 farXscale(0.0f);
+	glm::vec3 farYscale(0.0f);
+	glm::vec3 nearXscale(0.0f);
+	glm::vec3 nearYscale(0.0f);
+
+	m_cameraFront = glm::normalize(m_cameraFront);
+	nearPoint[0] = m_cameraFront * m_near;
+	farPoint[0] = m_cameraFront * m_far;
+
+	farYscale.y = m_far * tan(m_fov / 2.0f);
+	farXscale.x = m_aspect * farYscale.y;
+
+	nearYscale.y = m_near * tan(m_fov / 2.0f);
+	nearXscale.x = m_aspect * nearYscale.y;
+
+	farPoint[1] = toWorldMatrix * glm::vec4((farPoint[0] + farYscale - farXscale), 1.0f);
+	farPoint[2] = toWorldMatrix * glm::vec4((farPoint[0] - farYscale - farXscale), 1.0f);
+	farPoint[3] = toWorldMatrix * glm::vec4((farPoint[0] - farYscale + farXscale), 1.0f);
+	farPoint[4] = toWorldMatrix * glm::vec4((farPoint[0] + farYscale + farXscale), 1.0f);
+
+	nearPoint[1] = toWorldMatrix * glm::vec4((nearPoint[0] + nearYscale - nearXscale), 1.0f);
+	nearPoint[2] = toWorldMatrix * glm::vec4((nearPoint[0] - nearYscale - nearXscale), 1.0f);
+	nearPoint[3] = toWorldMatrix * glm::vec4((nearPoint[0] - nearYscale + nearXscale), 1.0f);
+	nearPoint[4] = toWorldMatrix * glm::vec4((nearPoint[0] + nearYscale + nearXscale), 1.0f);
+
+	m_frustum.plane[0] = FrustumPlane(nearPoint[1], nearPoint[2], nearPoint[3]);
+	m_frustum.plane[1] = FrustumPlane(farPoint[4], farPoint[3], farPoint[2]);
+	m_frustum.plane[2] = FrustumPlane(farPoint[1], farPoint[2], nearPoint[2]);
+	m_frustum.plane[3] = FrustumPlane(nearPoint[4], nearPoint[3], farPoint[3]);
+	m_frustum.plane[4] = FrustumPlane(farPoint[1], nearPoint[1], nearPoint[4]);
+	m_frustum.plane[5] = FrustumPlane(farPoint[3], nearPoint[3], nearPoint[2]);
+
+	return m_frustum;
 }
 
 } // namespace ale
