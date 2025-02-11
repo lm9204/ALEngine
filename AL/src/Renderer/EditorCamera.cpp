36959e852fe47e552ad7a60fca9a286fa5da93e4
seonjo1 <tjdgus9684@gmail.com>
2025-02-11 15:57:46 +0900

    [Fix]: change getFrustum to virtual function in EditorCamera

--- AL/src/Renderer/EditorCamera.cpp
+++ AL/src/Renderer/EditorCamera.cpp
@@ -105,4 +105,81 @@ void EditorCamera::updateView()
 	setViewMatrix(m_cameraPos, m_cameraFront, m_cameraUp);
 }
 
+
+const Frustum &EditorCamera::getFrustum()
+{
+	updateView();
+	glm::mat4 toWorldMatrix = glm::inverse(m_view);
+
+	// 0: center, 1: leftUp, 2: leftDown, 3: rightDown, 4: rightUp
+	glm::vec3 farPoint[5];
+	glm::vec3 nearPoint[5];
+
+	// 0: Y, 1: X
+	glm::vec3 farXscale(0.0f);
+	glm::vec3 farYscale(0.0f);
+	glm::vec3 nearXscale(0.0f);
+	glm::vec3 nearYscale(0.0f);
+
+	nearPoint[0] = glm::vec3(0.0f, 0.0f, -1.0f) * m_near;
+	farPoint[0] = glm::vec3(0.0f, 0.0f, -1.0f) * m_far;
+
+	// AL_CORE_INFO("near: {}", m_near);
+	// AL_CORE_INFO("far: {}", m_far);
+	// AL_CORE_INFO("fov: {}", m_fov);
+	// AL_CORE_INFO("aspect: {}", m_aspect);
+	// AL_CORE_INFO("cameraPos: {}, {}, {}", m_cameraPos.x, m_cameraPos.y, m_cameraPos.z);
+	// AL_CORE_INFO("cameraFront: {}, {}, {}", m_cameraFront.x, m_cameraFront.y, m_cameraFront.z);
+	// AL_CORE_INFO("cameraUp: {}, {}, {}", m_cameraUp.x, m_cameraUp.y, m_cameraUp.z);
+
+
+	farYscale.y = m_far * tan(m_fov / 2.0f);
+	farXscale.x = m_aspect * farYscale.y;
+
+	nearYscale.y = m_near * tan(m_fov / 2.0f);
+	nearXscale.x = m_aspect * nearYscale.y;
+
+	farPoint[1] = toWorldMatrix * glm::vec4((farPoint[0] + farYscale - farXscale), 1.0f);
+	farPoint[2] = toWorldMatrix * glm::vec4((farPoint[0] - farYscale - farXscale), 1.0f);
+	farPoint[3] = toWorldMatrix * glm::vec4((farPoint[0] - farYscale + farXscale), 1.0f);
+	farPoint[4] = toWorldMatrix * glm::vec4((farPoint[0] + farYscale + farXscale), 1.0f);
+
+	// AL_CORE_INFO("\n\n\nfar!!");
+	// for (int i = 1; i < 5; i++)
+	// {
+	// 	AL_CORE_INFO("farPoint[{}]", i);
+	// 	AL_CORE_INFO("{}, {}, {}", farPoint[i].x, farPoint[i].y, farPoint[i].z);
+	// }
+
+	nearPoint[1] = toWorldMatrix * glm::vec4((nearPoint[0] + nearYscale - nearXscale), 1.0f);
+	nearPoint[2] = toWorldMatrix * glm::vec4((nearPoint[0] - nearYscale - nearXscale), 1.0f);
+	nearPoint[3] = toWorldMatrix * glm::vec4((nearPoint[0] - nearYscale + nearXscale), 1.0f);
+	nearPoint[4] = toWorldMatrix * glm::vec4((nearPoint[0] + nearYscale + nearXscale), 1.0f);
+
+
+	// AL_CORE_INFO("\n\n\nnear!!");
+	// for (int i = 1; i < 5; i++)
+	// {
+	// 	AL_CORE_INFO("nearPoint[{}]", i);
+	// 	AL_CORE_INFO("{}, {}, {}", nearPoint[i].x, nearPoint[i].y, nearPoint[i].z);
+	// }
+
+	m_frustum.plane[0] = FrustumPlane(nearPoint[1], nearPoint[2], nearPoint[3]);
+	m_frustum.plane[1] = FrustumPlane(farPoint[4], farPoint[3], farPoint[2]);
+	m_frustum.plane[2] = FrustumPlane(farPoint[1], farPoint[2], nearPoint[2]);
+	m_frustum.plane[3] = FrustumPlane(nearPoint[4], nearPoint[3], farPoint[3]);
+	m_frustum.plane[4] = FrustumPlane(farPoint[1], nearPoint[1], nearPoint[4]);
+	m_frustum.plane[5] = FrustumPlane(farPoint[3], nearPoint[3], nearPoint[2]);
+
+	// AL_CORE_INFO("\n\n\nplane!!");
+	// for (int i = 0; i < 6; i++)
+	// {
+	// 	AL_CORE_INFO("plane[{}]", i);
+	// 	AL_CORE_INFO("normal : {}, {}, {}", m_frustum.plane[i].normal.x, m_frustum.plane[i].normal.y, m_frustum.plane[i].normal.z);
+	// 	AL_CORE_INFO("distance: {}", m_frustum.plane[i].distance);
+	// }
+
+	return m_frustum;
+}
+
 } // namespace ale
