b8051a52ee95ea25f5e5cd62bec0a72ac3011217
jgoo <maruluv0527@gmail.com>
2025-02-12 16:59:21 +0900

    [Feat]: Material UI added

--- Sandbox/src/Panel/SceneHierarchyPanel.cpp
+++ Sandbox/src/Panel/SceneHierarchyPanel.cpp
@@ -683,6 +683,34 @@ void SceneHierarchyPanel::drawComponents(Entity entity)
 			}
 			ImGui::EndDragDropTarget();
 		}
+		auto &materials = rc->getMaterials();
+
+		if (materials.size() != 1)
+		{
+			std::shared_ptr<Material> material = materials[1];
+			Albedo &albedo = material->getAlbedo();
+			drawVec3Control("Albedo", albedo.albedo);
+			drawCheckBox("Albedo Flag", albedo.flag);
+
+			NormalMap &normalMap = material->getNormalMap();
+			drawCheckBox("Normal Flag", normalMap.flag);
+
+			Roughness &roughness = material->getRoughness();
+			drawFloatControl("Roughness", roughness.roughness);
+			drawCheckBox("Roughness Flag", roughness.flag);
+
+			Metallic &metalic = material->getMetallic();
+			drawFloatControl("Metallic", metalic.metallic);
+			drawCheckBox("Metallic Flag", metalic.flag);
+
+			AOMap &aoMap = material->getAOMap();
+			drawFloatControl("AOMap", aoMap.ao);
+			drawCheckBox("AOMap Flag", aoMap.flag);
+
+			HeightMap &heightMap = material->getHeightMap();
+			drawFloatControl("HeightMap", heightMap.height);
+			drawCheckBox("HeightMap Flag", heightMap.flag);
+		}
 	});
 
 	drawComponent<LightComponent>("Light", entity, [entity, scene = m_Context](auto &component) mutable {
